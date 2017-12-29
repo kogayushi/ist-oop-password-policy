3946d289823b44dc6b97f57c45dafe20f38e2e0a
kogayushi <youshe.old.pleasure@gmail.com>
2017-12-29 23:33:50 +0900

    ユーザ名も変更可能にしたい
    重複を排除

--- src/main/java/com/example/ist/domain/model/policy/PolicyFactory.java
+++ src/main/java/com/example/ist/domain/model/policy/PolicyFactory.java
@@ -13,6 +13,20 @@ public class PolicyFactory {
 
     @SuppressWarnings("unchecked")
     public PasswordPolicy generatePasswordPolicyFor(User user) {
+        Set<Policy> policies = generateCommonPolicy(user);
+
+        return new PasswordPolicy(policies);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public UsernamePolicy generateUsernamePolicyFor(User user) {
+        Set<Policy> policies = generateCommonPolicy(user);
+
+        return new UsernamePolicy(policies);
+    }
+
+    private Set<Policy> generateCommonPolicy(User user) {
         Set<Policy> policies = new LinkedHashSet<>();
 
         policies.add(new LengthPolicy(8, 20));
@@ -29,29 +43,6 @@ public class PolicyFactory {
         ContactInformation contactInformation = person.getContactInformation();
         policies.add(new NotSameWithMailAddressPolicy(contactInformation.getMailAddress()));
         policies.add(new NotSameWithTelephonNumberPolicy(contactInformation.getTelephoneNumber()));
-
-        return new PasswordPolicy(policies);
-    }
-
-    @SuppressWarnings("unchecked")
-    public UsernamePolicy generateUsernamePolicyFor(User user) {
-        Set<Policy> policies = new LinkedHashSet<>();
-
-        policies.add(new LengthPolicy(8, 20));
-        // Compositeパターン refer -> https://ja.wikipedia.org/wiki/Composite_%E3%83%91%E3%82%BF%E3%83%BC%E3%83%B3
-        policies.add(new CharacterCombinationPolicy());
-
-        policies.add(new NotSameWithCurrentUsernamePolicy(user.getUsername()));
-        policies.add(new NotSameWithCurrentPasswordPolicy(user.getUsername()));
-
-        Person person = user.getPerson();
-
-        policies.add(new NotIncludingNamePolicy(person.getFirstName(), person.getLastName()));
-
-        ContactInformation contactInformation = person.getContactInformation();
-        policies.add(new NotSameWithMailAddressPolicy(contactInformation.getMailAddress()));
-        policies.add(new NotSameWithTelephonNumberPolicy(contactInformation.getTelephoneNumber()));
-
-        return new UsernamePolicy(policies);
+        return policies;
     }
 }
